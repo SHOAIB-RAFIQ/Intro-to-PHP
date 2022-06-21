4053baf87fac8f090b2f9ba7bfc9600aa600af6b
Shoaib Rafique <srafique6177@gmail.com>
2022-06-22 01:06:40 +0500

    Steps to make changes in the local and show them in repo on github

--- Functions/Functions.php
+++ Functions/Functions.php
@@ -1,2 +1,10 @@
-<?php
-?>
+1.make a repo on github
+2.clone the repo on local in htdocs
+3.setting name and email of git
+git config --global user.email "srafique6177@gmail.com"
+git config --global user.name "Shoaib rafique"
+4.making changes in folder
+5.git status of files
+6.git add .
+7.git commit -m "abc"
+8.git push origin main
